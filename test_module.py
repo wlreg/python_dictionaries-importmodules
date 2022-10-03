cb3ad2bdd9514d29959c4c035cd844023302430d
Billy Lacey <william.lacey23r@gmail.com>
2022-10-03 15:15:18 -0400

    first commit

---
+++ test_module.py
@@ -0,0 +1,11 @@
+
+def square_footage_finder(length, width):
+    square_feet = int(length) * int(width)
+    print(square_feet)
+
+
+import math 
+
+def circle_circumfrence(radius):
+    circumfrence = 2 * math.pi * radius
+    print(circumfrence)
